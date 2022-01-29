2351757bf5ab24be2bb09cbb3df3bb8af66efb3e
yjzhu1106 <yjzhu1106@163.com>
2022-01-30 00:20:48 +0800

    add testChaincode

---
+++ chaincode/go/testChaincode/testChaincode.go
@@ -0,0 +1,24 @@
+package testChaincode
+
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
+import (
+	"log"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+
+
+
+func main() {
+	assetChaincode, err := contractapi.NewChaincode(&SmartContract{})
+	if err != nil {
+		log.Panicf("Error creating asset-transfer-basic chaincode: %v", err)
+	}
+
+	if err := assetChaincode.Start(); err != nil {
+		log.Panicf("Error starting asset-transfer-basic chaincode: %v", err)
+	}
+}
